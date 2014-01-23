3cbf5931dcdf5b5fa50ab2c54eee27ce2771ea72
Gary Fredericks <garyf@groupon.com>
2014-01-23 17:49:24 +0000

    Update the hell out of clojure

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
 (defproject com.gfredericks/re-rand "0.1.0"
   :description "Generates random strings from regular expressions"
-  :dependencies [[org.clojure/clojure "1.1.0"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]])
