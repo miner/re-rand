252086f03cf77e13c4fb707300bf769182081d8b
Gary Fredericks <garyf@groupon.com>
2014-01-23 18:54:34 +0000

    Version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.gfredericks/re-rand "0.1.0"
+(defproject com.gfredericks/re-rand "0.1.1"
   :description "Generates random strings from regular expressions"
   :dependencies [[com.gfredericks/four "0.2.0"]
                  [org.clojure/clojure "1.5.1"]])
