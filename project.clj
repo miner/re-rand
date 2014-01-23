871c11bad686e1b5772d3113bf73c216c9ac482c
Gary Fredericks <garyf@groupon.com>
2014-01-23 17:48:23 +0000

    Switch to my maven group name

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject re-rand "0.1.0"
+(defproject com.gfredericks/re-rand "0.1.0"
   :description "Generates random strings from regular expressions"
   :dependencies [[org.clojure/clojure "1.1.0"]]
   :dev-dependencies [[lein-clojars "0.5.0"]])
