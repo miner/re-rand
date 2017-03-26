cbaedbbd36754802ca5c9526f784bd02daecc23f
Steve Miner <steveminer@gmail.com>
2017-03-26 18:16:33 -0400

    fixed typo in group

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.velicso/re-rand "0.1.2"
+(defproject com.velisco/re-rand "0.1.3"
   :description "Generates random strings from regular expressions"
   :dependencies [[com.gfredericks/four "0.2.0"]
                  [org.clojure/clojure "1.6.0"]])
